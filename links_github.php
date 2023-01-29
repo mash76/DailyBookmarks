7d372b09ac6eacef41a5140b2d0e818e1bdc6f90
Mash76 <masatohori76@gmail.com>
2023-01-29 14:41:25 +0900

    first
    first

---
+++ links_github.php
@@ -0,0 +1,225 @@
+<html>
+<head>
+	<meta http-equiv="content-language" content="ja" charset="UTF-8">
+    <title>links</title>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<style>
+		body{color:#666; font-family:sans-serif,helvetica; }
+		a:link{color:#1e90ff; text-decoration:none; }
+		a:visited{color:#1e90ff; text-decoration:none; }
+		a:hover{color:#1e90ff; text-decoration:underline;}
+
+        .top_nowrap {vertical-align:top; white-space:nowrap;}
+		silver {color:silver; }
+
+		.flex-item {margin-left:15px; margin-bottom:20px; width:300px; }
+        .flex-container {display: flex; flex-wrap:wrap; }
+	</style>
+</head>
+<body>
+    <div class="flex-container">
+        <div class="flex-item" > 
+            <b>生活</b> <hr/>
+            <a target="_blank" sc="syukujitu" href="https://www.google.com/search?q=%E4%BB%8A%E5%B9%B4%E3%81%AE%E7%A5%9D%E6%97%A5">祝日</a>
+            <a target="_blank" sc="ondo" href="https://www.google.com/search?q=%E6%B8%A9%E5%BA%A6">温度</a>  
+
+            <br/>
+            <a target="_blank" sc="hotpepper" href="https://beauty.hotpepper.jp/CSP/my/myPage/">HotPepperBeauty</a>
+            <br/>
+
+            googleツール 
+            <a target="_blank" sc="doc" href="https://docs.google.com/document/">Doc</a> 
+            <a target="_blank" sc="sheet" href="https://docs.google.com/spreadsheets/">Sheet</a>  
+            <a target="_blank" sc="gmail" href="https://mail.google.com/mail/u/0/?pli=1#inbox">GMail</a>
+            <a target="_blank" sc="calender" href="https://calendar.google.com/calendar/u/0/r/month">Calender</a>
+
+        </div>
+        <div class="flex-item" > 
+            <b>マネー</b> <hr/>
+            相場          
+            <a target="_blank" sc="nikkei225" href="https://www.google.com/search?q=nikkei225">Nikkei225</a>   
+            <a target="_blank" sc="usdjpy" href="https://www.google.com/search?q=usdjpy">USDJPY</a>   
+            <br/>
+            銀行
+            <a target="_blank" sc="paypaybank" href="https://www.paypaybank.co.jp/">PayPay銀行</a>
+            <a target="_blank" sc="yuutyo" href="https://direct.jp-bank.japanpost.jp/tp1web/U010101WAK.do?link_id=ycDctLgn">ゆうちょ</a>
+            <br/>
+
+        </div>
+
+        <div class="flex-item" > 
+            <b>配送など</b> <hr/>
+            配送 
+            <b><a target="_blank" sc="yamato" href="https://cmypage.kuronekoyamato.co.jp/portal/entrance?id=kojintop">ヤマト</a></b>
+
+            <a target="_blank" href="http://www.kuronekoyamato.co.jp/ytc/customer/send/services/takkyubin/">宅急便</a> 
+            <silver>3辺60-200cm</silver> 
+            <br/>
+
+            <a target="_blank" sc="sagawa" href="https://www.e-service.sagawa-exp.co.jp/portal/do/login/show"><b>佐川</b></a>
+            <a target="_blank" href="https://www.e-service.sagawa-exp.co.jp/e/do/su/uke/input/show">集荷</a>
+            <br/>
+
+            郵便局
+            <a target="_blank" href="https://webyubin.jpi.post.japanpost.jp/webyubin/snt/DYFR900.do">Webゆうびん/速達</a>
+            <a target="_blank" sc="clickpost" href="https://clickpost.jp/">クリックポスト</a> <silver>188円 34x25x5 印刷可</silver>
+            
+    
+            <br/>
+
+            公共料金 
+            <a target="_blank" sc="gas" href="https://members.tokyo-gas.co.jp/">東京ガス</a> 
+            <br/>
+
+        </div>
+
+        <div class="flex-item" >   
+            <b>サブスク課金確認(頭にs)</b> <hr/>
+                Amazon 
+                    <a target="_blank" sc="skindle" href="https://www.amazon.co.jp/kindle-dbs/ku/ku-central?_encoding=UTF8&ref_=d2_ss_kindle_unlimited">KindleUL</a>
+                    <a target="_blank" sc="smusic" href="https://www.amazon.co.jp/music/settings">MusicUL</a>
+                    <br/>
+                動画
+                    <a target="_blank" href="https://www.dmm.co.jp/my/-/serviceinfo/">dmm</a> 
+                    <a target="_blank" sc="netflix" href="https://www.netflix.com/jp/">netflix</a> 
+                    <a target="_blank" sc="syoutube" href="https://www.youtube.com/paid_memberships">Youtubeプレミアム</a>
+                    <a target="_blank" sc="sspotify" href="https://www.spotify.com/jp/account/subscription/">spotify</a> 
+                    <br/>
+                書籍
+                    <a target="_blank" sc="sdmagazine" href="https://d.dmkt-sp.jp/common/mypage_setting/index.html">d-magazine</a><br/>
+                ポータル
+                    <a target="_blank" sc="syahoo" href="https://premium.yahoo.co.jp/">Yahooプレミアム</a><br/>
+
+                Saas
+                <a target="_blank" sc="sdropbox" href="https://www.dropbox.com/account/plan">Dropbox</a>
+                <a target="_blank" href="https://www.evernote.com/Settings.action/">Evernote</a>             
+
+                <br/>
+                開発    
+                    <a target="_blank" sc="sadobe" href="https://account.adobe.com/plans">adobe</a>    
+            
+
+
+
+        </div>
+
+
+        <div class="flex-item" >
+            <b>東京</b> <hr/>
+            <a target="_blank" sc="bunbetsu" href="https://www.city.shinjuku.lg.jp/content/000259254.pdf">新宿ごみ分別 </a>
+            <a target="_blank" sc="sodai" href="http://sodai.tokyokankyo.or.jp/Sodai/V2WebNotes/13104/0/">粗大ごみ</a>
+            <br/>
+
+            <br/>フリマ<br/>
+            <a target="_blank" sc="mercali" href="https://www.mercari.com/jp/">mercali</a> 
+            <a target="_blank" sc="msyu" href="https://jp.mercari.com/mypage/listings">出品</a>
+            <a target="_blank" sc="mkou" href="https://jp.mercari.com/mypage/purchases">購入</a> 
+
+            <br/>
+            <a target="_blank" sc="yafu" href="https://auctions.yahoo.co.jp/">ヤフオク</a>   
+            <a target="_blank" sc="jimoty" href="https://jmty.jp/web_mail/posts">ジモティ</a>                  
+            <br/>
+            <a target="_blank" sc="amazon" href="http://amazon.co.jp">amazon</a>
+        </div>
+
+        <div class="flex-item" >    
+                <b>娯楽</b> <hr/>
+
+                CD/DVDレンタル
+                <a target="_blank" sc="discus" href="https://movie-tsutaya.tsite.jp/netdvd/dvd/top.do?i=0&pT=0">DISCUS</a>
+                <a target="_blank" sc="geo" href="https://rental.geo-online.co.jp/">GEO</a>  
+
+                <br/>
+                Book
+                <a target="_blank" sc="dmagazine" href="https://magazine.dmkt-sp.jp/">dマガジン</a>
+                <br/>
+                Game 
+                <a target="_blank" sc="wordle" href="https://www.nytimes.com/games/wordle/index.html">wordle</a>
+                <a target="_blank" sc="sushida" href="https://sushida.net/">寿司打</a>
+
+                
+
+        </div>
+        <div class="flex-item" >    
+            <b>SNS</b> <hr/>
+            <a sc="fb" href="https://www.facebook.com/">facebook</a>
+            <a sc="twitter" href="https://twitter.com/home">twitter</a>
+        </div>  
+        
+        <div class="flex-item" >   
+            <b>婚活</b> <hr/>
+            pairs
+                <a target="_blank" sc="pairs" href="https://pairs.lv/#/message">探す</a>	
+                <a target="_blank" sc="message" href="https://pairs.lv/#/message">メッセ</a>
+                <a target="_blank" sc="community" href="https://pairs.lv/#/community">コミュニティ</a>
+        </div>
+
+    </div> <!-- flex container -->
+
+    <!-- 入力表示ウィンドウ -->
+    <div id="enter" style="position:absolute; border-radius:20px; width:600px; 
+                            opacity:0.8; top:200px; left:400px;
+                            padding-left:20px; padding-right:20px; padding-bottom:20px;">
+        <div id="input_words" style="font-size:600%; "></div>
+        <table><tr>
+            <td id="kouho_shortcuts" style="font-size:150%; color:gray; "></td>
+            <td id="kouho_lists" style="font-size:150%; "></td>
+        </tr></table>
+    </div>
+    <script>
+
+        let input_chars = ""
+        let is_input_allow = true
+        $(document).ready(function() {
+            // ショートカットキー
+            $(document).keydown(function(event){ 
+
+                let kouho_names = []
+                let kouho_shortcuts =[]
+
+                if (event.key === 'Enter' ) input_chars = "" //enterで入力リセット
+                if (event.key.length == 1 ) input_chars += event.key.toLowerCase() //一文字ならok metaやshift enterはx
+                // 一つだけ該当すれば遷移
+                $('a').css('border','').css('background','') 
+                let matchs = $('a[sc^=' + escapeJquerySelector(input_chars) + ']')
+                if (matchs.length == 1) location.href = $(matchs[0]).attr('href') // 1件絞れたらなら開く 
+                if (matchs.length == 0) is_input_allow = false // マッチ0なら追加入力不可
+
+                console.log('len' ,matchs.length )
+                matchs.each(function(ind,obj){
+                    let shortcut_str = $(obj).attr('sc').toLowerCase()
+                        kouho_shortcuts.push( (kouho_names.length + 1) + ' ' + shortcut_str.replace(input_chars,'<b>' + input_chars + '</b>')) //マッチ分太字
+                        kouho_names.push($(obj).text())
+                        $(obj).css('border','1px solid red').css('background','#fff8f8')            
+                })
+                showKouhos(kouho_names,kouho_shortcuts)
+            })
+        })
+
+        // 入力中文字列と候補リストを表示/非表示
+        function showKouhos(kouho_names,kouho_shortcuts){
+
+            if (input_chars.length == 0 ) {
+                $('#enter').css('display','none')
+            }else{
+                $('#enter').css('display','block')                
+            }
+            $('#input_words').html(input_chars );
+            if (kouho_names.length == 0 ) {
+                $('#enter').css('background','red').css('color','white')
+            }else{
+                $('#enter').css('background','#f0f0ff').css('color','')       
+            }
+            $('#kouho_lists').html(kouho_names.join('<br/>'))
+            $('#kouho_shortcuts').html(kouho_shortcuts.join('<br/>'))
+        }
+
+        // jqueryにセットする用にエスケープ id="val[1]" の項目を選択するときなど
+        function escapeJquerySelector(val){
+            return val.replace(/[ !"#$%&'()*+,.\/:;<=>?@\[\\\]^`{|}~]/g, "\\$&");
+        }   
+    </script>
+
+
+</body>
+</html>
